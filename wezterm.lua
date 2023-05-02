8ca3b422c905021c910700b8c86639d19a37f67a
Ming <455747495@qq.com>
2023-05-02 12:16:23 +0800

    'init'

--- wezterm.lua
+++ wezterm.lua
@@ -1,12 +1,88 @@
 local wezterm = require 'wezterm'
 local config = {}
+local launch_menu = {}
+
 
 
 config.colors = {
+   -- The default text color
+   foreground = 'silver',
+   -- The default background color
+   background = 'black',
+ 
+   -- Overrides the cell background color when the current cell is occupied by the
+   -- cursor and the cursor style is set to Block
+   cursor_bg = '#52ad70',
+   -- Overrides the text color when the current cell is occupied by the cursor
+   cursor_fg = 'black',
+   -- Specifies the border color of the cursor when the cursor style is set to Block,
+   -- or the color of the vertical or horizontal bar when the cursor style is set to
+   -- Bar or Underline.
+   cursor_border = '#52ad70',
+ 
+   -- the foreground color of selected text
+   selection_fg = 'black',
+   -- the background color of selected text
+   selection_bg = '#fffacd',
+ 
+   -- The color of the scrollbar "thumb"; the portion that represents the current viewport
+   scrollbar_thumb = '#222222',
+ 
+   -- The color of the split lines between panes
+   split = '#444444',
+ 
+   ansi = {
+     'black',
+     'maroon',
+     'green',
+     'olive',
+     'navy',
+     'purple',
+     'teal',
+     'silver',
+   },
+   brights = {
+     'grey',
+     'red',
+     'lime',
+     'yellow',
+     'blue',
+     'fuchsia',
+     'aqua',
+     'white',
+   },
+ 
+   -- Arbitrary colors of the palette in the range from 16 to 255
+   indexed = { [136] = '#af8700' },
+ 
+   -- Since: 20220319-142410-0fcdea07
+   -- When the IME, a dead key or a leader key are being processed and are effectively
+   -- holding input pending the result of input composition, change the cursor
+   -- to this color to give a visual cue about the compose state.
+   compose_cursor = 'orange',
+ 
+   -- Colors for copy_mode and quick_select
+   -- available since: 20220807-113146-c2fee766
+   -- In copy_mode, the color of the active text is:
+   -- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
+   -- 2. selection_* otherwise
+   copy_mode_active_highlight_bg = { Color = '#000000' },
+   -- use `AnsiColor` to specify one of the ansi color palette values
+   -- (index 0-15) using one of the names "Black", "Maroon", "Green",
+   --  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
+   -- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
+   copy_mode_active_highlight_fg = { AnsiColor = 'Black' },
+   copy_mode_inactive_highlight_bg = { Color = '#52ad70' },
+   copy_mode_inactive_highlight_fg = { AnsiColor = 'White' },
+ 
+   quick_select_label_bg = { Color = 'peru' },
+   quick_select_label_fg = { Color = '#ffffff' },
+   quick_select_match_bg = { AnsiColor = 'Navy' },
+   quick_select_match_fg = { Color = '#ffffff' },
   tab_bar = {
     -- The color of the strip that goes along the top of the window
     -- (does not apply when fancy tab bar is in use)
-    background = '#0b0022',
+    background = '#fff',
 
     -- The active tab is the one that has focus in the window
     active_tab = {
@@ -75,7 +151,6 @@ config.colors = {
     },
   },
 }
-
 config.window_background_image = './forest.jpg'
 
 config.window_background_image_hsb = {
@@ -89,6 +164,46 @@ config.window_background_image_hsb = {
   -- You can adjust the saturation also.
   saturation = 1.0,
 }
+config.keys = {
+  { key = 'z', mods = 'ALT', action = wezterm.action.ShowLauncher },
+}
+
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  ssh_cmd = { "powershell.exe" }
+
+  table.insert(
+    launch_menu,
+    {
+      label = "PowerShell",
+      args = { "powershell.exe", "-NoLogo" }
+    }
+  )
+
+  table.insert(
+    launch_menu,
+    {
+      label = "Bash",
+      args = { "D:/Program Files/Git/bin/bash.exe" }
+    }
+  )
+
+  table.insert(
+    launch_menu,
+    {
+      label = "CMD",
+      args = { "cmd.exe" }
+    }
+  )
+end
+
+
+config.launch_menu = launch_menu
+config.window_background_image = 'C:/Users/Administrator/.config/wezterm/images/code-wallpaper-6.png'
+config.window_background_opacity = 0.9
+config.color_scheme = 'AdventureTime'
+
+-- config.default_prog = {'porwershell'}
 
 
 return config
